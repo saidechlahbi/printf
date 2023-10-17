f11c02482099fda41134b7bf1153d0ef573057fd
saidechlahbi <echlahbisaid@gmail.com>
2023-10-17 04:56:53 -0700

    update

--- _printf.c
+++ _printf.c
@@ -1,154 +1,49 @@
 #include "main.h"
+#include <stdarg.h>
+
+
 /**
- * if_specifier - Function that compare the specifier to get.
- * the function of the specifier.
- * @spec: character argument.
+ * _printf - home made printf
  *
- * @s: character argument.
- * Return: i to get the first index from the spec[i].
+ * @format: array of things to be printed
  *
- */
-
-int if_specifier(choose_tt spec[], char s)
-{
-	int l;
-
-	for (l = 0; l < 12; l++)
-	{
-		if (s == spec[l].c)
-			return (l);
-	}
-	return (-5);
-}
-
-/**
- * string - Function that print String.
- *
- *@arg: Variadic Arguments from user [String]
- *
- * Return: Length of string
- */
-
-int string(va_list arg)
-{
-	char *s = va_arg(arg, char*);
-
-	if (s == NULL)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-	else
-	{
-		write(1, s, ((int)strlen(s)));
-	}
-	return ((int)strlen(s));
-}
-/**
- * ch - Function that print characters.
- *@arg: Variadic Arguments from user [character]
- * Return: Length 1 that equal one character
- */
-int ch(va_list arg)
-{
-	char s = va_arg(arg, int);
-
-	write(1, &s, 1);
-	return (1);
-}
-
-/**
- * decimal - Function that print Numbers.
- *@arg: Variadic Arguments from user [Int].
- * Return: Length of digits
- */
-
-int decimal(va_list arg)
-{
-	int r = va_arg(arg, int);
-	int len = 0;
-	unsigned int d, i, count;
-
-	if (r == 0)
-	{
-		write(1, "0", 1);
-		return (++len);
-	}
-	if (r < 0)
-	{
-		write(1, "-", 1);
-		len++;
-		i = r * -1;
-	}
-	else
-	{
-		i = r;
-	}
-	d = i;
-	count = 1;
-	while (d > 9)
-	{
-		d /= 10;
-		count *= 10;
-	}
-	while (count >= 1)
-	{
-		char s = ((i / count) % 10) + '0';
-
-		write(1, &s, 1);
-		len++;
-		count /= 10;
-	}
-	return (len);
-}
-
-/**
- * _printf - Function that print the format with handling the specifier.
- *@format: Fixed argument that take specifier.
- * Return: Length of format.
+ * Return: number of characters printed
  */
 
 int _printf(const char *format, ...)
 {
-	va_list arg;
-	int len = 0, f = 0;
-	int spec_idx;
+	va_list ap;
+	int count;
 
-	choose_tt spec[] = {{'c', ch}, {'s', string}, {'d', decimal},
-{'i', decimal}, {'r', reverse}, {'R', rot13}, {'b', binary},
-{'X', hex_upper}, {'x', hex_lower}, {'o', octal}, {'u', un_int}, {'p', po}};
+	func_printer printers[] = {
+		{"c", pr_char},
+		{"s", pr_string},
+		{"%", pr_percent},
+		{"i", pr_int},
+		{"d", pr_int},
+		{"b", pr_binary},
+		{"u", pr_unsigned_int},
+		{"o", pr_octal},
+		{"x", pr_hexa},
+		{"X", pr_heXa},
+		{"S", pr_string_special},
+		{"p", pr_pointer},
+		{"r", pr_reverse},
+		{"R", pr_rot13},
+		{NULL, NULL}
+	};
+
 	if (format == NULL)
-		return (-1);
-	va_start(arg, format);
-	while (format[f] && format)
 	{
-		spec_idx = if_specifier(spec, format[f + 1]);
-		if ((format[f] == '%'))
-		{
-			if (format[f + 1] == '\0')
-				return (-1);
-			else if (format[f + 1] == '%')
-			{
-				write(1, "%", 1);
-				len++;
-				f += 2;
-			} else if (spec_idx >= 0)
-			{
-				len += spec[spec_idx].ptr(arg);
-				f += 2;
-			} else
-			{
-				len++;
-				write(1, &(format[f]), 1);
-				f++;
-			}
-		} else
-		{
-			len++;
-			write(1, &(format[f]), 1);
-			f++;
-		}
+		return (-1);
 	}
-	return (len);
+
+	va_start(ap, format);
+
+	count = prf_looper(format, printers, ap);
+
+	va_end(ap);
+
+	return (count);
 }
 
