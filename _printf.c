709d7b2862d80030197a9f5258a8275b4fbd393f
saidechlahbi <echlahbisaid@gmail.com>
2023-10-17 14:22:22 -0700

    update

--- _printf.c
+++ _printf.c
@@ -19,15 +19,6 @@ int _printf(const char *format, ...)
 		{"%", pr_percent},
 		{"i", pr_int},
 		{"d", pr_int},
-		{"b", pr_binary},
-		{"u", pr_unsigned_int},
-		{"o", pr_octal},
-		{"x", pr_hexa},
-		{"X", pr_heXa},
-		{"S", pr_string_special},
-		{"p", pr_pointer},
-		{"r", pr_reverse},
-		{"R", pr_rot13},
 		{NULL, NULL}
 	};
 
